beabb887ac65584d24d58a7200abbe285923f0f6
ryo-murai <murai.ry@gmail.com>
2021-08-16 19:17:37 +0900

    generated by spring initializr

---
+++ studyawslambda/src/main/java/org/example/studyawslambda/StudyawslambdaApplication.java
@@ -0,0 +1,13 @@
+package org.example.studyawslambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyawslambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyawslambdaApplication.class, args);
+	}
+
+}
---
+++ studyawslambda/src/test/java/org/example/studyawslambda/StudyawslambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studyawslambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyawslambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
